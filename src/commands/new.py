30b7094eda02b48246ca1661aca9f709e919d81f
Ubus99 <>
2024-02-26 01:43:46 +0100

    implemented "new" command

--- src/commands/new.py
+++ src/commands/new.py
@@ -1,4 +1,16 @@
 import logging
+from pathlib import Path
+import shutil
+
 
 def init(args):
-    logging.error("function not implemented")
+    src_path = Path("./data/buildscript.json").resolve()
+    dst_dir = args.path.resolve()
+    dst_src_dir = dst_dir / "src"
+    dst_path = dst_dir / "buildscript.json"
+
+    logging.info("initializing PyLuaLinker project to " + str(dst_path))
+    dst_src_dir.mkdir(parents=True)
+    shutil.copyfile(src_path, dst_path)
+
+    logging.info("Finished")
