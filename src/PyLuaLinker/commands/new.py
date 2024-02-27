fffdb2af13c0341e80dd52edffd6deb2931e3080
Ubus99 <>
2024-02-27 02:49:17 +0100

    fixed init command

--- src/PyLuaLinker/commands/new.py
+++ src/PyLuaLinker/commands/new.py
@@ -2,15 +2,13 @@ import logging
 from pathlib import Path
 import shutil
 
-
 def init(args):
-    src_path = Path("./data/buildscript.json").resolve()
+    src_path = (Path(__file__) / "../../../data/buildscript.json").resolve()
     dst_dir = args.path.resolve()
     dst_src_dir = dst_dir / "src"
-    dst_path = dst_dir / "buildscript.json"
 
-    logging.info("initializing PyLuaLinker project to " + str(dst_path))
-    dst_src_dir.mkdir(parents=True)
-    shutil.copyfile(src_path, dst_path)
+    logging.info("initializing PyLuaLinker project to " + str(dst_dir))
+    dst_src_dir.mkdir(parents=True, exist_ok=True)
+    shutil.copy(src_path, dst_dir)
 
     logging.info("Finished")
