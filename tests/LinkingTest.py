d8f5f8d706f1dce561c9a4b0ef7f9e5a7282bb18
Lukas Berghegger <lukasberghegger@gmail.com>
2024-03-04 16:17:41 +0100

    implemented test cases

--- tests/LinkingTest.py
+++ tests/LinkingTest.py
@@ -1,6 +1,17 @@
+import argparse
 import unittest
+from pathlib import Path
+
+import PyLuaLinker.commands.build as build
 
 
 class LinkingTest(unittest.TestCase):
+
+    def setUp(self):
+        self.target_path = Path("./project/buildscript.json")
+
     def runTest(self):
+        build.build(argparse.Namespace(path=self.target_path))
+
+    def tearDown(self):
         pass
---
+++ tests/TestProjectInitialization.py
@@ -0,0 +1,32 @@
+import argparse
+import shutil
+import unittest
+from pathlib import Path
+
+import PyLuaLinker.commands.new as new
+
+
+class TestProjectInitialization(unittest.TestCase):
+
+    def setUp(self):
+        self.target_path = Path("./" + __name__ + "/")
+
+    def test_new(self):
+        new.new_project(argparse.Namespace(path=self.target_path))
+
+        bs_path = Path(self.target_path / "buildscript.json").resolve()
+        bs_template_path = Path("../src/data/buildscript.json").resolve()
+
+        with (bs_path.open() as bs,
+              bs_template_path.open() as bs_template):
+            bs_string = bs.read()
+            bs_template_string = bs_template.read()
+
+            self.assertEqual(bs_template_string, bs_string)
+
+    def tearDown(self):
+        shutil.rmtree(self.target_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
